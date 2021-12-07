450e9ee8b0323ac02eee22f250b751354749bd70
latifahsabilil <71626855+latifahsabilil@users.noreply.github.com>
2021-12-07 11:31:46 +0700

    Create MPV.java

---
+++ BahanBakar/src/bahanbakar/MPV.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bahanbakar;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class MPV extends BBM{
+    public double jarak, kecepatan;
+    
+    public double hitungbahanBakar(){
+        double bbm = this.jarak / 14;
+        return bbm;
+    }
+    
+    public double hitungwaktu(){
+        double waktu = this.jarak / this.kecepatan;
+        return waktu;
+    }
+}
